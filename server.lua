8d854e8612115d6994c812b4f59aa0e678eeca72
Eden Sun <huntack478@gmail.com>
2017-01-31 15:06:06 +0800

    pararrel send files, cool

--- server.lua
+++ server.lua
@@ -5,24 +5,25 @@ HEADER200 = "HTTP/1.1 200 OK\r\n"..
             "Content-Type: text/html; charset=UTF-8\r\n\r\n"
 HEADER404 = "HTTP/1.0 404 Not Found\r\n\r\nPage not found"
 
-sendPayload = function(fopened, cli)
-  print(fopened)
-  local tem = file.read()
+sendPayload = function(fil, cli)
+  local tem = fil:read()
   if tem then
     print('len = ',string.len(tem))
-    cli:send(tem, function () sendPayload(fopened, cli) end)
+    cli:send(tem, function () sendPayload(fil, cli) end)
   else
     print('close.')
     cli:close()
   end
 end
+
 srv:listen(80,function(conn)
     conn:on("receive", function(client,payload)
         local tarFile = string.sub(payload,string.find(payload,"GET /") +5,string.find(payload,"HTTP/") -2 )
         if tarFile == "" then tarFile = "index.html" end
-        local f = file.open(tarFile, "r")
-        if f ~= nil then
-            client:send(HEADER200, function () sendPayload(f, client) end)
+        local fff = file.open(tarFile, "r")
+        if fff then
+            print(26,fff)
+            client:send(HEADER200, function () sendPayload(fff, client) end)
         else
             client:send(HEADER404, function () client:close() end)
         end
