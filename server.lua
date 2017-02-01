10e11ae0cddbecd8f50415e39b95305275c73e47
Eden Sun <huntack478@gmail.com>
2017-02-01 16:19:13 +0800

    handle next packet request

--- server.lua
+++ server.lua
@@ -42,45 +42,47 @@ JSON = 1
 ENURL = 2
 function handlePOST (client, payload)
     local cut, tail = payload:find('\r\n\r\n')
-    if cut then
-        local cType = ENURL
-        local header = payload:sub(1,cut)
-        local body = payload:sub(tail)
-        for key, val in header:gmatch("(%S+):([^\n]+)") do
-            if key == 'Content-Type' then
-                cType = val:find('json') and JSON or ENURL
-            end
+    if cut == nil then client:close() return end
+    local cType = ENURL
+    local header = payload:sub(1,cut)
+    local body = payload:sub(tail)
+    for key, val in header:gmatch("(%S+):([^\n]+)") do
+        if key == 'Content-Type' or key == 'content-type' then
+            cType = val:find('json') and JSON or ENURL
         end
-        print('type = ',cType)
-        local params = {}
-        for k, v in pairs(params) do
-            print(k,v)
-        end
-        if cType == JSON then
-            local t = cjson.decode(body)
-            for k, v in pairs(t) do params[k] = v end
-        else
-            for k, v in body:gmatch("(%w+)=(%w+)") do params[k] = v end
-        end
-        client:send(HEADERJSON, function()
-            local json = {}
-            for pin = 0, 8 do
-                local nVal = tonumber(params[pin])
-                if nVal and pin > 5 then
-                    gpio.write(pin, nVal == 1 and gpio.LOW or gpio.HIGH)
-                    print(74, nVal)
-                elseif nVal then
-                    print(76, nVal)
-                    gpio.write(pin, nVal == 1 and gpio.HIGH or gpio.LOW)
-                end
-                json[pin] = gpio.read(pin)
-            end
-            client:send(cjson.encode(json), function () client:close() end)
-            client:close()
-        end)
-    else
-        client:send("no params!!!", function() client:close() end)
     end
+    print('type = ',cType, cut, tail, payload:len())
+    if tail == payload:len() then -- has next packet
+      client:on("receive", function (client, nextPayload)
+        handlePOST(client, payload..nextPayload)
+      end)
+      return
+    end
+    local params = {}
+    for k, v in pairs(params) do
+        print(k,v)
+    end
+    if cType == JSON then
+        local t = cjson.decode(body)
+        for k, v in pairs(t) do params[tonumber(k)] = v end
+    else
+        for k, v in body:gmatch("(%w+)=(%w+)") do params[tonumber(k)] = v end
+    end
+    client:send(HEADERJSON, function()
+        local json = {}
+        for pin = 0, 8 do
+            local nVal = tonumber(params[pin])
+            if nVal and pin > 5 then
+                gpio.write(pin, nVal == 1 and gpio.HIGH or gpio.LOW)
+            elseif nVal then
+                gpio.write(pin, nVal == 1 and gpio.LOW or gpio.HIGH)
+            end
+            json[pin] = gpio.read(pin)
+            if pin <= 5 then json[pin] = json[pin] == 1 and 0 or 1 end
+        end
+        client:send(cjson.encode(json), function () client:close() end)
+        client:close()
+    end)
 end
 srv:listen(80,function(conn)
     conn:on("receive", function(client, payload)
